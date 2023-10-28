8199a35186551354fe8ae009f9571a3083bf8d0d
Dorito Cat <doritocat2015@gmail.com>
2023-10-27 21:45:17 -0400

    UI tweaks and bug fixes

--- src/LocalBusinessDirectory/tailwind.config.js
+++ src/LocalBusinessDirectory/tailwind.config.js
@@ -8,45 +8,52 @@ module.exports = {
         "./*.razor",
         "./Shared/**/*.razor"
     ],
-  theme: {
-      extend: {
-          fontFamily: {
-              'default': ['Onest', 'Helvetica', 'Arial', 'sans-serif']
-          },
-          minWidth: {
-              '1/4': "25%",
-              '1/2': "50%",
-              ...defaultTheme.spacing
-          },
-          minHeight: {
-              ...defaultTheme.spacing
-          },
-          maxWidth: {
-              '1/2': "50%",
-              '3/4': "75%",
-              ...defaultTheme.spacing
-          },
-          maxHeight: {
-              '3/4': "75%"
-          },
-          animation: {
-              'enlarge': 'enlarge 0.2s cubic-bezier(0.42, 0, 0.63, 1.77) forwards',
-              'enlarge-small': 'enlarge-small 0.2s cubic-bezier(0.42, 0, 0.63, 1.77) forwards'
-          },
-          keyframes: {
-              enlarge: {
-                  '0%': { '--tw-scale-x': '.97', '--tw-scale-y': '.97' },
-                  '50%': { '--tw-scale-x': '1.03', '--tw-scale-y': '1.03' },
-                  '100%': { '--tw-scale-x': '1', '--tw-scale-y': '1' }
-              },
-              'enlarge-small': {
-                  '0%': { '--tw-scale-x': '.99', '--tw-scale-y': '.99' },
-                  '50%': { '--tw-scale-x': '1.01', '--tw-scale-y': '1.01' },
-                  '100%': { '--tw-scale-x': '1', '--tw-scale-y': '1' }
-              }
-          }
+    theme: {
+        extend: {
+            fontFamily: {
+                'default': ['Onest', 'Helvetica', 'Arial', 'sans-serif'],
+                'display': ['Space Mono', 'Helvetica', 'Arial', 'sans-serif']
+            },
+            minWidth: {
+                '1/4': "25%",
+                '1/2': "50%",
+                ...defaultTheme.spacing
+            },
+            minHeight: {
+                ...defaultTheme.spacing
+            },
+            maxWidth: {
+                '1/2': "50%",
+                '3/4': "75%",
+                ...defaultTheme.spacing
+            },
+            maxHeight: {
+                '3/4': "75%"
+            },
+            animation: {
+                'enlarge': 'enlarge 0.2s cubic-bezier(0.42, 0, 0.63, 1.77) forwards',
+                'enlarge-small': 'enlarge-small 0.2s cubic-bezier(0.42, 0, 0.63, 1.77) forwards',
+                'text-blinker': '1.5s infinite text-blinker'
+            },
+            keyframes: {
+                enlarge: {
+                    '0%': { '--tw-scale-x': '.97', '--tw-scale-y': '.97' },
+                    '50%': { '--tw-scale-x': '1.03', '--tw-scale-y': '1.03' },
+                    '100%': { '--tw-scale-x': '1', '--tw-scale-y': '1' }
+                },
+                'enlarge-small': {
+                    '0%': { '--tw-scale-x': '.99', '--tw-scale-y': '.99' },
+                    '50%': { '--tw-scale-x': '1.01', '--tw-scale-y': '1.01' },
+                    '100%': { '--tw-scale-x': '1', '--tw-scale-y': '1' }
+                },
+                'text-blinker': {
+                    '0%': { 'opacity': 1 },
+                    '50%': { 'opacity': 0 },
+                    '100%': { 'opacity': 1 }
+                }
+            }
         },
-  },
-  plugins: [],
+    },
+    plugins: [],
 }
 
